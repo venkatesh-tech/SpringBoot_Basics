a93a71f5a0ab5b529f0adc91de8c66eae35dddc2
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-16 18:58:39 +0530

    Added Actuators

--- src/main/java/com/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ src/main/java/com/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,20 @@ package com.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class,
+				args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()) { // Prints all the beans present in the
+																			// application
+			System.out.println(name);
+		}
 	}
 
 }
